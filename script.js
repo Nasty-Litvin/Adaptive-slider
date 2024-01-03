ab44b943368f892cbf3719f1f36384fe327aa7d9
nasty <nastylitvin1991liverpool@gmail.com>
2024-01-03 22:51:59 +0300

    Create adaptive slider.

---
+++ script.js
@@ -0,0 +1,37 @@
+const images = document.querySelectorAll(".slider .slider-line img");
+const slider = document.querySelector(".slider");
+const sliderLine = document.querySelector(".slider-line");
+const btnPrevious = document.querySelector(".slider-previous");
+const btnNext = document.querySelector(".slider-next");
+let count = 0;
+let width;
+
+function init() {
+  console.log("resize");
+  width = slider.offsetWidth;
+  images.forEach((item) => {
+    item.style.width = width + "px";
+    item.style.height = "auto";
+  });
+  rollSlider();
+}
+window.addEventListener("resize", init);
+init();
+
+btnPrevious.addEventListener("click", function () {
+    count--;
+    if (count < 0) {
+      count = 0;
+    }
+    rollSlider();
+  });
+btnNext.addEventListener("click", function () {
+  count++;
+  if (count >= images.length) {
+    count = images.length-1;
+  }
+  rollSlider();
+});
+function rollSlider() {
+  sliderLine.style.transform = "translate(-" + count * width + "px)";
+}
